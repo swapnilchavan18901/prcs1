2066a773db9f6cf7b50e800a3141e0eeaf9a2675
Swapnil Umesh Chavan <78468955+swapnilchavan18901@users.noreply.github.com>
2023-04-19 02:09:46 +0530

    Add files via upload

---
+++ INSERT~1.C
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+	key = arr[i];
+	j = i - 1;
+
+	while (j >= 0 && arr[j] > key) {
+	    arr[j + 1] = arr[j];
+	    j = j - 1;
+	}
+	arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int n, i,arr[100];
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++) {
+	scanf("%d", &arr[i]);
+    }
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: \n");
+    for (i = 0; i < n; i++)
+	printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
+
